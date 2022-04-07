fd1713e156963c79d73837e83d02cd7c12be7857
MohaimanulIslam <mohaimanul.cse@gmail.com>
2022-04-07 21:45:25 +0600

    solve some issue

--- cypress/integration/utils/locators.js
+++ cypress/integration/utils/locators.js
@@ -1,4 +1,5 @@
 class Login{
+    
     clickLoginBtn = '//*[@id="__PWS_ROOT__"]/div[1]/div/div/main/div[1]/div[1]/div[2]/div[2]/button/div';
     email = '//*[@id="email"]';
     pass = '//*[@id="password"]';
